650f305ff5dbabf9e3322b103a3e053402fa94e0
rajesh-kayal-dev <rk6289943975@gmail.com>
2025-07-09 12:25:44 +0530

    Fix: update start script and port handling

--- db.js
+++ db.js
@@ -5,10 +5,7 @@ require('dotenv').config();
 const mongoURL = process.env.MONGODB_URL;
 
 
-mongoose.connect(mongoURL,{
-    useNewUrlParser: true,
-    useUnifiedTopology: true
-})
+mongoose.connect(mongoURL)
 
 const db = mongoose.connection;
 
